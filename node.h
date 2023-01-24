266239a0dbf5179004be87a2f9e8ff6f1fd2b393
dpv927 <dpv927@inlumine.ual.es>
2023-01-24 20:57:59 +0100

    Update node class

--- node.h
+++ node.h
@@ -1,3 +1,4 @@
+//#include "nodeComparator.h" 
 /** The Node class is used for representing the different Nodes or states of a
  * board in a seach tree. The node must have a self heuristic, a data matrix with
  * the state of the board in that node and its depth inside the seach tree.
@@ -5,13 +6,17 @@
  * @version 23.01.2023
  * @see nodeImpl.cpp to see how this class functions are implemented. */
 class Node {
+  // Just a reminder for the Node class that NodeComparator exists. 
+  friend struct NodeComparator;    
+
   public:
     static int EXPLORED_NODES; // Count of all the explored nodes
     static int EXPANDED_NODES; // Count of all the expanded nodes
     static Node* ROOT_NODE; // Root Node of a search tree (Initial state)
     static Node* FINAL_NODE; // Final Node of a search tree (target state)
     static Coordinate FINAL_POS[9]; // Final positions of the chips
-
+    static struct NodeComparator comparator; // Comparator for two nodes 
+  
     Node* father; // Father node
     uint8_t** data; // Game board
     int nodeDepth; // Depth of this node
@@ -21,7 +26,7 @@ class Node {
     * Creates a intermidiate Node of a search tree.
     * @param data Board state of the created Node
     * @param father Father node of this Node in the seach tree */
-    Node(uint8_t** data, Node* father) {
+    Node(Node* father, uint8_t** data) {
       this->data = data;
       this->father = father;
       this->nodeDepth = father->nodeDepth + 1;
@@ -42,20 +47,20 @@ class Node {
     static void initFinalPositions(std::string);
   /* @brief Initializes the root and final node of a seach */
     static void initNodes(std::string, std::string);
-   /* @brief Prints the Node. */
+  /* @brief Generates the sucessors of a node. */
+    std::priority_queue<Node*, std::vector<Node*>, NodeComparator> generateSucessors();
+  /* @brief Prints the Node. */
     void toString();
-   /* @brief Checks if a node is equal to another. */
+  /* @brief Checks if a node is equal to another. */
     bool equals(Node*);
 
   private:
     /*@brief Calculates the heuristic of a node*/
     int calculateHeuristic();
     /*@brief Copies the board state of a Node.*/
-    uint8_t** dataCopy(uint8_t** data);
+    uint8_t** dataCopy();
 };
 
-/* A simple comparator between Nodes. Useful when using priority queques
- * with nodes at the search algorithms */
 struct NodeComparator {
     bool operator()(const Node* a, const Node* b) {
       return a->eval > b->eval;
