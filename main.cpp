17eb62f83aa5cfb80b2600daed47fe6f797d6f1d
dpv927 <dpv927@inlumine.ual.es>
2023-01-24 02:26:49 +0100

    -

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
-#include "node.h"
-#include <iostream>
+#include<iostream>
+#include<string>
+//#include "node.h"
+//#include <queue>
+//#include <vector>
 
 int main() {
-    // Creación de los punteros a las filas de la matriz
-    uint8_t** data = new uint8_t*[3];
-    for (int i = 0; i < 3; i++) {
-        data[i] = new uint8_t[3];
-    }
-
-    // Inicialización de los valores de la matriz
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            data[i][j] = 1;
-        }
-    }
+  /*std::priority_queue<Node*, std::vector<Node*>, NodeComparator> pq;
   
-    // Creación de un objeto de tipo Node
-    Node* node = new Node(data);
-    node->toString();
-    return 0;
+  uint8_t** data = nullptr;
+  Node* father = nullptr;
+  Node node(data, father);
+  pq.push(&node);
+*/
+  std::string cad = "123456789";
+  for(char var : cad) {
+    std::cout << var << std::endl;
+  }
+
+  return 0;
 }
 
