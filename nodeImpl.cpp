84f5fed80842e4c1495ce05d464e1d1bbcbd6b83
dpv927 <dpv927@inlumine.ual.es>
2023-01-24 20:58:44 +0100

    Create node sucessor generator function

--- nodeImpl.cpp
+++ nodeImpl.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <string>
 #include <cstdint>
+#include <queue>
+#include <vector>
 #include "coordinate.h"
 #include "node.h"
 
@@ -38,22 +40,79 @@ void initNodes(std::string init, std::string final) {
   Node::FINAL_NODE = new Node(finData);
 }
 
+/** @brief Generates all the sucessors of a node (by its legal moves) and
+ * stores them in a priority queue. The orther is the next one: First we
+ * try to move the chip that is above the gap, then the one under the gap,
+ * next the one at the left and lastly the one at the right. */
+std::priority_queue<Node*, std::vector<Node*>, NodeComparator> Node::generateSucessors() {
+  std::priority_queue<Node*, std::vector<Node*>, NodeComparator> queue;
+  int holeRow = 0, holeCol = 0;
+  
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if(this->data[i][j] == 0) {
+        holeRow = i;
+        holeCol = i;
+        break;
+      }        
+    }
+  }
+
+  if(holeRow > 0) { // move up to down
+	  Node* node = new Node(this, this->dataCopy());
+		node->data[holeRow][holeCol] = node->data[holeRow-1][holeCol];
+		node->data[holeRow-1][holeCol] = 0;
+		queue.push(node);
+  }
+
+	if(holeRow < 2) { // move down to up
+	  Node* node = new Node(this, this->dataCopy());
+		node->data[holeRow][holeCol] = node->data[holeRow+1][holeCol];
+		node->data[holeRow+1][holeCol] = 0;
+		queue.push(node);
+	}
+			
+	if(holeCol > 0) { // move left to right
+		Node* node = new Node(this, this->dataCopy());
+		node->data[holeRow][holeCol] = node->data[holeRow][holeCol-1];
+		node->data[holeRow][holeCol-1] = 0;
+		queue.push(node);
+	}
+	
+	if(holeCol < 2) { // move right to left
+		Node* node = new Node(this, this->dataCopy());
+		node->data[holeRow][holeCol] = node->data[holeRow][holeCol+1];
+		node->data[holeRow][holeCol+1] = 0;
+		queue.push(node);
+	}
+  return queue;
+}
+
 /* @brief Calculates the heuristic of a Node.
  * @returns Value of the heuristic */
 int Node::calculateHeuristic() {
-  int total = 0;   
+  int total = 0;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; i++) {
+      Coordinate* target = &Node::FINAL_POS[this->data[i][j]];
+      
+      if(i != target->i || j != target->j) {
+        total += (abs(target->i - i) + abs(target->j - j)) + 1;
+      }
+    }
+  }
   return total;
 }
 
 /* @brief Utility to copy a Node's data (board)
  * @param data Data to copy 
  * @return Copied data */
-uint8_t** Node::dataCopy(uint8_t** data) {
+uint8_t** Node::dataCopy() {
   uint8_t** dataCopy = new uint8_t*[3];
 
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; i++) {
-      dataCopy[i][j] = data[i][j];
+      dataCopy[i][j] = this->data[i][j];
     }
   }
   return dataCopy;
@@ -63,8 +122,9 @@ uint8_t** Node::dataCopy(uint8_t** data) {
 bool Node::equals(Node* other) {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; i++) {
-      if(this->data[i][j] != other->data[i][j])
+      if(this->data[i][j] != other->data[i][j]) {
         return false;
+      }
     }
   }
   return true;
