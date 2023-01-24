947f08ea8c4c3086ceca0645d3813f2c791e0aed
dpv927 <dpv927@inlumine.ual.es>
2023-01-24 20:57:28 +0100

    testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include<iostream>
-#include<string>
+//#include<iostream>
+//#include<string>
+//#include<cstdint>
+//#include<cmath>
 //#include "node.h"
 //#include <queue>
 //#include <vector>
@@ -11,11 +13,13 @@ int main() {
   Node* father = nullptr;
   Node node(data, father);
   pq.push(&node);
-*/
+
   std::string cad = "123456789";
   for(char var : cad) {
     std::cout << var << std::endl;
-  }
+  }*/
+
+  
 
   return 0;
 }
