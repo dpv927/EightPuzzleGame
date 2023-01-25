ceeba299436f43378c3d0598cca9fef06a444608
dpv927 <dpv927@inlumine.ual.es>
2023-01-25 21:16:10 +0100

    Finish first version

--- main.cpp
+++ main.cpp
@@ -1,26 +1,60 @@
-//#include<iostream>
-//#include<string>
-//#include<cstdint>
-//#include<cmath>
-//#include "node.h"
-//#include <queue>
-//#include <vector>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <functional>
+#include "coordinate.h"
+#include "node.h"
+#include "algorithm.h"
 
 int main() {
-  /*std::priority_queue<Node*, std::vector<Node*>, NodeComparator> pq;
+  std::string initial, final;
+  std::stack<Node*> solutionPath;
+  Node* solution;
+  Node* tmp;
+
+  std::cout << "Eight Puzzle Game Solver 1.0.0 - by Filipondios\n" <<
+  "Introduce a String that represents the board, describing the board and the position " <<
+  "of each chip from up to down and from left to right.\n>> ";  
+  std::cin >> initial;
+
+  std::cout << "\nIntroduce the final (target) board config:\n>> ";
+  std::cin >> final;
+
+  if(initial.length()!=9 || final.length()!=9) {
+    std::cout << "Cannot parse the given strings to a board config :(" << std::endl;
+    exit(0);
+  }
   
-  uint8_t** data = nullptr;
-  Node* father = nullptr;
-  Node node(data, father);
-  pq.push(&node);
+  Node::initNodes(initial, final);
+  Node::initFinalPositions(final);
 
-  std::string cad = "123456789";
-  for(char var : cad) {
-    std::cout << var << std::endl;
-  }*/
-
+  solution = AStar();
+    
+  if(solution==nullptr) {
+    std::cout << "\nNo solution found." << std::endl;
+    exit(0);
+  }
   
+  std::cout << "\nInitial board state: " << std::endl;
+  Node::ROOT_NODE->toString();
+  std::cout << "\nFinal board state:" << std::endl;
+  Node::FINAL_NODE->toString();
+  std::cout << std::endl;
 
+  solutionPath = Node::getAncestorsQueue(solution);
+  std::cout << "Solution path: \n" << std::endl;
+
+  while(!solutionPath.empty()) {
+    tmp = solutionPath.top();
+    tmp->toString();
+    std::cout << "Depth = " << tmp->nodeDepth << "\n" << std::endl;
+    solutionPath.pop();
+  }  
+  
+  std::cout << "Solution found at depth: " << solution->nodeDepth << "\nCreated Nodes: " 
+            << Node::CREATED_NODES << "\nExpanded nodes: " << Node::EXPANDED_NODES << "\n";
   return 0;
 }
-
